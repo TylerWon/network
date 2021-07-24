edd2dbd2ca9c45f65bcefd41d12bd862ec1e5b97
TylerWon <tywon@shaw.ca>
2021-07-23 22:08:06 -0700

    change Follower model to be a field of the User model

--- network/models.py
+++ network/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Represents data for a User in the User table of the database
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField("User")
 
 # Represents data for a Post in the Post table of the database
 class Post(models.Model):
@@ -13,9 +13,5 @@ class Post(models.Model):
 
 # Represents data for a Like in the Like table of the database
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    liker = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
-
-# Represents data for a Follower in the Follower table of the database
-class Follower(models.Model):
-    followee = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
