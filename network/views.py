8bc38b78af50ac67c672bdf96bedfe8f30f90fc7
TylerWon <tywon@shaw.ca>
2021-07-24 17:30:28 -0700

    create API route that facilitates the creation of a new post

--- network/views.py
+++ network/views.py
@@ -1,16 +1,19 @@
+import json
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post, Like
 
-
+# Render the index page
 def index(request):
     return render(request, "network/index.html")
 
-
+# Render the login page
 def login_view(request):
     if request.method == "POST":
 
@@ -30,12 +33,12 @@ def login_view(request):
     else:
         return render(request, "network/login.html")
 
-
+# Log a user out
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
-
+# Render the register page
 def register(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -61,3 +64,24 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+# API Route: create a new post
+@login_required
+def new_post(request):
+    # Check request method was POST, reject otherwise
+    if request.method != "POST":
+        return JsonResponse({"error": "POST request required"}, status=400)
+    
+    data = json.loads(request.body)
+
+    # Check content is non-empty, reject otherwise
+    content = data.get("content").strip()
+    if content == "":
+        return JsonResponse({"error": "Content of post cannot be empty"}, status=400)
+
+    user = request.user
+
+    post = Post(poster=user, content=content)
+    post.save()
+
+    return JsonResponse({"message": "Post created sucessfully"}, status=201)
